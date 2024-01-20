577d8b1a70cd3f834658cef5bcd1ce886e78250f
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2024-01-20 12:13:20 +0000

    mandatory

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,14 +8,13 @@
 */
 void bubble_sort(int *array, size_t size)
 {
-	int swap = 1, c = 0;
+	int c = 0;
 	size_t i = 0, j = 0;
 
 	if (array == NULL || size < 2)
 		return;
 	for (i = 0; i < size; i++)
 	{
-		swap = 0;
 		c = 0;
 		for (j = 0; j < size - 1; j++)
 		{
@@ -25,7 +24,6 @@ void bubble_sort(int *array, size_t size)
 				array[j] = array[j + 1];
 				array[j + 1] = c;
 				print_array(array, size);
-				swap = 1;
 			}
 		}
 	}
