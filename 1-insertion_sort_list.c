0af4ca27cd8e837442e8bd5a0ddefed8ab3a59f9
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2024-01-20 22:44:13 +0000

    mandatory

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -13,7 +13,7 @@ void insertion_sort_list(listint_t **list)
 	listint_t *ref = *list;
 
 
-	if (list == NULL)
+	if (list == NULL || *list == NULL || *list->next == NULL)
 		return;
 	nex = nex->next;
 	ref = ref->next;
