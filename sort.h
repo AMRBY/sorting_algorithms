9821691061ec0b7b5bd1496f900c84047af6e8d2
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2024-01-20 12:16:53 +0000

    mandatory

--- sort.h
+++ sort.h
@@ -1,3 +1,5 @@
+#ifndef "SORT_H"
+#define "SORT_H"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -19,3 +21,4 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
+#endif
