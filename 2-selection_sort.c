e563ab99fc7eae14b76088599550d19485310dad
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2024-01-21 22:24:53 +0000

    mandatory

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,34 +1,66 @@
 #include "sort.h"
-/**
-* selection_sort - sorting array
-* Description: it's a selection sort
-* @array: listint_t
-* @size: size
-* return: list
-*/
-void selection_sort(int *array, size_t size)
-{
-	size_t i, j, tmp;
-	int low;
 
-	if (array == NULL || size < 2)
+/**
+ * swap_nodes - swaps two nodes in a doubly linked list
+ * @list: doubly-linked list
+ * @curr: node currently indexed
+ * @prev: node before curr
+*/
+void swap_nodes(listint_t **list, listint_t *curr, listint_t *prev)
+{
+	listint_t *temp;
+
+	if (prev->prev != NULL)
+		prev->prev->next = curr;
+	if (curr->next != NULL)
+		curr->next->prev = prev;
+
+	temp = prev->prev;
+	prev->prev = curr;
+	prev->next = curr->next;
+	curr->prev = temp;
+	curr->next = prev;
+	if (temp == NULL)
+		*list = curr;
+
+}
+/**
+ * insertion_sort_list - sorts a linked list using insertion sort algorithm
+ * @list: double pointer to a linked list
+*/
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *curr;
+	listint_t *prev;
+	int i = 0;
+	listint_t *stop;
+
+	/*case when the list is empty or has one node*/
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
-	for (j = 0; j < size - 1; j++)
+	prev = *list;
+	curr = (*list)->next;
+	while (curr != NULL)
 	{
-		low = array[j];
-		for (i = j + 1; i < size; i++)
+		i = 0; /*tracks position of first swap in each iteration*/
+		stop = curr;
+swap:
+		if (prev != NULL && curr->n < prev->n)
 		{
-			if (array[i] < low)
-			{
-				low = array[i];
-				tmp = i;
-			}
+			swap_nodes(list, curr, prev);
+			print_list(*list);
+			++i;
+			if (i == 1)
+				stop = prev;
 		}
-		if (low != array[j])
+		else
 		{
-			array[tmp] = array[j];
-			array[j] = low;
-			print_array(array, size);
+			curr = stop->next;
+			if (curr != NULL)
+				prev = curr->prev;
+			continue;
 		}
+		prev = curr->prev;
+		goto swap;
 	}
 }
